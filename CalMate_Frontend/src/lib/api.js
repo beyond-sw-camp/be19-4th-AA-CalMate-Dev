c8686fd5593e4e630e9d44fa9ef0f536a3a8cc20
HyeonggyuKang <kin0204@naver.com>
2025-11-14 09:51:00 +0900

    k8s배포를위해 url경로수정

--- CalMate_Frontend/src/lib/api.js
+++ CalMate_Frontend/src/lib/api.js
@@ -38,14 +38,14 @@ const formattedTimeForKor =  () => {
 const api = axios.create({
   // baseURL: API_BASE_URL,          // 모든 상대 경로 요청은 '/localhost:80'를 기준으로 보낸다. (Vite dev proxy로 백엔드 연결 가정)
   // baseURL: 'http://localhost:80/back',          // 모든 상대 경로 요청은 '/localhost:80'를 기준으로 보낸다. (Vite dev proxy로 백엔드 연결 가정)
-  // baseURL: '/back',          // 모든 상대 경로 요청은 '/localhost:80'를 기준으로 보낸다. (Vite dev proxy로 백엔드 연결 가정)
-  baseURL: 'http://localhost:8081',          // 모든 상대 경로 요청은 '/localhost:80'를 기준으로 보낸다. (Vite dev proxy로 백엔드 연결 가정)
+  baseURL: '/back',          // 모든 상대 경로 요청은 '/localhost:80'를 기준으로 보낸다. (Vite dev proxy로 백엔드 연결 가정)
+  // baseURL: 'http://localhost:8081',          // 모든 상대 경로 요청은 '/localhost:80'를 기준으로 보낸다. (Vite dev proxy로 백엔드 연결 가정)
   withCredentials: true,    // ✅ 브라우저가 HttpOnly 쿠키(리프레시 토큰)를 자동으로 전송하도록 허용
   timeout: 15000,           // 네트워크 요청 타임아웃(ms). 필요에 따라 조정 가능.
 });
 
 export const API_BASE_URL = api.defaults.baseURL;
-console.log('API_BASE_URL::' , API_BASE_URL);
+// console.log('API_BASE_URL::' , API_BASE_URL);
 
 
 // ─────────────────────────────────────────────────────────────────────────────
